8222f40e93f1ba5644024371dcc69ffc8f6fbf47
InYuusha <asuse975@gmail.com>
2021-02-28 05:09:21 +0530

    finished post

--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -2,16 +2,18 @@
 <v-container style="position:absolute; top:10%">
      <h2>Add a Post</h2>
     <v-row align="center" style="margin-top:20px">
-        <form style="width:60%" action="http://127.0.0.1:5000/post" method="post">
+        <form style="width:60%" action="http://127.0.0.1:5000/post" method="post" enctype="multipart/form-data">
        
 
         <v-file-input name="img" type="file" outlined accept="image/" label="Selct a Cover"></v-file-input>
         <v-text-field  name="name" type="text" clearable outlined dark placeholder="Title"></v-text-field>
          <v-textarea name="description" v-model="description" type="text" auto-grow outlined clearable color="white" placeholder="Description"></v-textarea>
-         <v-text-field   id="keywords" v-model="tag" @keyup="ifSpace" clearable outlined dark placeholder="Tags"></v-text-field>
+         <v-text-field   v-model="tag" @keyup="ifSpace" clearable outlined dark placeholder="Tags"> </v-text-field>
+        
         <div><v-chip v-for="(tag,key) in tags" dark>{{tag}}<v-icon @click="removeTag(tag)">mdi-close-circle</v-icon></v-chip></div>
         <br>
         <v-btn type="submit" >Post</v-btn>
+         <v-text-field type="hidden" name="keywords" v-for="(tag,key) in tags" :value="tag"></v-text-field>
 
       </form>    
     
